1b426a256cf1babacfeca8f46457684bfde7460f
SamSam <sam40230@gmail.com>
2023-05-02 15:57:51 +0200

    added subject

--- src/app/state/event.service.ts
+++ src/app/state/event.service.ts
@@ -1,11 +1,38 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { ActionEvent } from "./aircraft.state";
+import { ActionEvent, AircraftsActionTypes } from "./aircraft.state";
+import { AircraftService } from '../services/aircraft.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
 
-  constructor() { }
-}
+  eventSubject : Subject<ActionEvent> = new Subject<ActionEvent>();
+  eventSubjectObservable = this.eventSubject.asObservable();
+
+  publishEvent(event:ActionEvent) {
+
+    this.eventSubject.next(event);
+  }
+
+  constructor(private aircraftService:AircraftService, private eventService:EventService) {  }
+
+  ngOnInit() : void {
+
+    this.eventService.eventSubjectObservable.subscribe((actionEvent : ActionEvent) => {
+      this.onActionEvent(actionEvent);
+    }
+  )}
+  onActionEvent(actionEvent: ActionEvent) {
+    throw new Error('Method not implemented.');
+  }
+  getAllAircrafts() {
+    this.eventService.publishEvent({
+      type: AircraftsActionTypes.GET_ALL_AIRCRAFTS, payload: null,
+      $actionEvent: undefined
+    });
+
+    };
+  }
+

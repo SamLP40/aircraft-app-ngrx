0a17206e844ae42a551140527e8f1f9a67ebaeab
SamSam <sam40230@gmail.com>
2023-05-03 13:35:34 +0200

    added effects

--- src/app/ngrx/aircrafts.effects.ts
+++ src/app/ngrx/aircrafts.effects.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@angular/core";
 import { AircraftService } from "../services/aircraft.service";
-import { Actions } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { Action } from "@ngrx/store";
-import { Observable, catchError, map, mergeMap } from "rxjs";
-import { AircraftsActionTypes, GetAllAircraftsAction, GetAllAircraftsActionSuccess } from "./aircrafts.actions";
+import { Observable, catchError, map, mergeMap, of } from "rxjs";
+import { AircraftsActionTypes, GetAllAircraftsAction, GetAllAircraftsActionSuccess, GetAllAircraftsActionError } from "./aircrafts.actions";
 
 @Injectable() //décorateur utilisé pour désigner un service
 
@@ -12,12 +12,12 @@ export class AircraftsEffects {
 
     getAllAircraftsEffect:Observable<Action> = createEffect(
         () => this.effectActions.pipe(
-            ofType(AircraftsActionTypes.GET_ALL_AIRCRAFTS)
+            ofType(AircraftsActionTypes.GET_ALL_AIRCRAFTS),
             mergeMap((action)=> {
-                return this.aircraftService.getAircrafts.pipe(
+                return this.aircraftService.getAircrafts().pipe(
                     map((aircrafts) => new GetAllAircraftsActionSuccess(aircrafts)),
 
-                    catchError((err)=>of(new GetAllAircraftsActionError(errMessage)))
+                    catchError((err)=>of(new GetAllAircraftsActionError(err.message)))
                 )
             })
         )
